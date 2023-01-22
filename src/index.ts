c89f9e0d2218c9074af46138033b91b78835e7b4
AmritPaudel <46588187+amrit073@users.noreply.github.com>
2023-01-22 22:33:14 +0545

    fix ts errors

--- src/index.ts
+++ src/index.ts
@@ -88,6 +88,6 @@ router.all("*", () => new Response("404, not found!", { status: 404 }))
 This snippet ties our worker to the router we deifned above, all incoming requests
 are passed to the router where your routes are called and the response is sent.
 */
-addEventListener('fetch', (e) => {
+addEventListener('fetch', (e: FetchEvent) => {
 	e.respondWith(router.handle(e.request))
 })
--- src/parse.ts
+++ src/parse.ts
@@ -1,5 +1,19 @@
 import { XMLParser } from 'fast-xml-parser';
 
+interface Iitems {
+	title: string;
+	description: string;
+}
+
+interface Irss {
+	title: string;
+	description: string;
+	link: string;
+	image: string;
+	category: string;
+	items: Iitems[];
+};
+
 export default async (url: string) => {
 	if (!/(^http(s?):\/\/[^\s$.?#].[^\s]*)/i.test(url)) return null;
 
@@ -18,11 +32,11 @@ export default async (url: string) => {
 	let channel = result.rss && result.rss.channel ? result.rss.channel : result.feed;
 	if (Array.isArray(channel)) channel = channel[0];
 
-	const rss = {
+	const rss: Irss = {
 		title: channel.title || '',
 		description: channel.description || '',
 		link: channel.link && channel.link.href ? channel.link.href : channel.link,
-		image: channel.image ? channel.image.url : channel['itunes:image'] ? channel['itunes:image'].href : '',
+		image: channel.image ? channel.image.url : '',
 		category: channel.category || [],
 		items: [],
 	};
@@ -39,35 +53,7 @@ export default async (url: string) => {
 			title: val.title && val.title.$text ? val.title.$text : val.title,
 			description: val.summary && val.summary.$text ? val.summary.$text : val.description,
 			link: val.link && val.link.href ? val.link.href : val.link,
-			author: val.author && val.author.name ? val.author.name : val['dc:creator'],
-			published: val.created ? Date.parse(val.created) : val.pubDate ? Date.parse(val.pubDate) : Date.now(),
-			created: val.updated ? Date.parse(val.updated) : val.pubDate ? Date.parse(val.pubDate) : val.created ? Date.parse(val.created) : Date.now(),
-			category: val.category || [],
-			content: val.content && val.content.$text ? val.content.$text : val['content:encoded'],
-			enclosures: val.enclosure ? (Array.isArray(val.enclosure) ? val.enclosure : [val.enclosure]) : [],
 		};
-
-		['content:encoded', 'podcast:transcript', 'itunes:summary', 'itunes:author', 'itunes:explicit', 'itunes:duration', 'itunes:season', 'itunes:episode', 'itunes:episodeType', 'itunes:image'].forEach((s) => {
-			if (val[s]) obj[s.replace(':', '_')] = val[s];
-		});
-
-		if (val['media:thumbnail']) {
-			Object.assign(media, { thumbnail: val['media:thumbnail'] });
-			obj.enclosures.push(val['media:thumbnail']);
-		}
-
-		if (val['media:content']) {
-			Object.assign(media, { thumbnail: val['media:content'] });
-			obj.enclosures.push(val['media:content']);
-		}
-
-		if (val['media:group']) {
-			if (val['media:group']['media:title']) obj.title = val['media:group']['media:title'];
-
-			if (val['media:group']['media:description']) obj.description = val['media:group']['media:description'];
-
-			if (val['media:group']['media:thumbnail']) obj.enclosures.push(val['media:group']['media:thumbnail'].url);
-		}
 
 		Object.assign(obj, { media });
 
