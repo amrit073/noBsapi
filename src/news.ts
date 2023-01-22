bc9d7c01c6abc9ef661d932a872b639e3d0b0d23
AmritPaudel <46588187+amrit073@users.noreply.github.com>
2023-01-22 21:53:25 +0545

    news - only titles

--- src/news.ts
+++ src/news.ts
@@ -1,9 +1,30 @@
 import parser from './parse';
 
-export const getNews = async () => {
-	const rss = await parser('https://english.onlinekhabar.com/feed')
-	return JSON.stringify(rss, null, 3)
+var escapeChars = { lt: '<', gt: '>', quot: '"', apos: "'", amp: '&' };
+
+function unescapeHTML(str: string) {//modified from underscore.string and string.js
+	return str.replace(/\&([^;]+);/g, function(entity: string, entityCode: keyof typeof escapeChars) {
+		var match;
+
+		if (entityCode in escapeChars) {
+			return escapeChars[entityCode];
+		} else if (match = entityCode.match(/^#x([\da-fA-F]+)$/)) {
+			return String.fromCharCode(parseInt(match[1], 16));
+		} else if (match = entityCode.match(/^#(\d+)$/)) {
+			return String.fromCharCode(~~match[1]);
+		} else {
+			return entity;
+		}
+	});
 }
 
+export const getNews = async () => {
+	const rss = await parser('https://english.onlinekhabar.com/feed')
+	var titles: string = '';
+	rss?.items.forEach(e => {
+		titles = titles + `${e.title}\n`;
+	})
+	return unescapeHTML(titles);
+}
 // curl -s https://english.onlinekhabar.com/feed  | grep '<title>' | tail -n +3 | cut -d'>' -f2 | cut -d'<' -f1
 
